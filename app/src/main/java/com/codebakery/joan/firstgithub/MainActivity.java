5a0c09952865a8a6a3041576bd42e7367d89da23
codebaker <jungah.kim76@gmail.com>
2018-10-23 13:26:41 +0900

    3rd

---
+++ app/src/main/java/com/codebakery/joan/firstgithub/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codebakery.joan.firstgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+//깃헙사용해보기
+
+//또
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
